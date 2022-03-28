04311bf0531222f2e3cf7d55a652212815b826a5
saka <61535180+saka-naname@users.noreply.github.com>
2022-03-28 19:40:45 +0900

    いくつかファイルいじった

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const CopyFilePlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -14,9 +14,6 @@ const config = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
@@ -49,8 +46,6 @@ const config = {
 module.exports = () => {
   if (isProduction) {
     config.mode = "production";
-
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
   } else {
     config.mode = "development";
   }
