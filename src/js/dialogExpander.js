d28d7d1090ee4688bbfc112f21a66c6ac09659b0
saka <61535180+saka-naname@users.noreply.github.com>
2022-04-10 20:31:57 +0900

    お知らせダイアログの縦サイズを大きくする処理を実装 横サイズはいじらなかったけど可能

--- src/js/dialogExpander.js
+++ src/js/dialogExpander.js
@@ -5,7 +5,8 @@ const observer = new MutationObserver((mutations) => {
     const progressDialog = $("[aria-describedby='progress_dialog']");
     if(dialog.length > 0 && progressDialog.length === 0 && dialog.css("display") != "none"){
         //  ダイアログが開かれた時のみ処理を実行する
-        
+        $("#infoDetailView").css("max-height", "calc(90vh - 40px)").css("height", "calc(90vh - 40px)");
+        dialog.css("position", "fixed").css("inset", "0").css("margin", "auto").css("height", "fit-content");
     }
 });
 
