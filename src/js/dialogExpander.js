1d2ccb9c073fee55ec76e89698134ed958043b25
saka <61535180+saka-naname@users.noreply.github.com>
2022-04-12 21:55:19 +0900

    `lmsDialog`を`notificationDialog`にリネーム 理由：LMS以外でも使われるので

--- src/js/dialogExpander.js
+++ src/js/dialogExpander.js
@@ -3,7 +3,7 @@
 const observer = new MutationObserver((mutations) => {
     const dialog = $("[aria-describedby='infoDetailView']");
     const progressDialog = $("[aria-describedby='progress_dialog']");
-    const lmsDialog = $("[aria-describedby='info_detail_view2']");
+    const notificationDialog = $("[aria-describedby='info_detail_view2']");
 
     //  ポータルホームのダイアログの処理
     if(dialog.length > 0 && progressDialog.length === 0 && dialog.css("display") != "none"){
@@ -12,11 +12,11 @@ const observer = new MutationObserver((mutations) => {
         dialog.css("position", "fixed").css("inset", "0").css("margin", "auto").css("height", "fit-content");
     }
 
-    //  LMSのダイアログの処理
-    if(lmsDialog.length > 0 && lmsDialog.css("display") != "none"){
+    //  お知らせのダイアログの処理
+    if(notificationDialog.length > 0 && notificationDialog.css("display") != "none"){
         //  ダイアログが開かれた時のみ処理を実行する
         $("#info_detail_view2").css("max-height", "calc(90vh - 40px)").css("height", "calc(90vh - 40px)");
-        lmsDialog.css("position", "fixed").css("inset", "0").css("margin", "auto").css("height", "fit-content");
+        notificationDialog.css("position", "fixed").css("inset", "0").css("margin", "auto").css("height", "fit-content");
     }
 });
 
